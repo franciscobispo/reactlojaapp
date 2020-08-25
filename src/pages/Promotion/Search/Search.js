ada6afad97e001a077b2f1b038808f886e0fa03f
Francisco Cruz <francisco.bispocruz@gmail.com>
2020-08-25 18:18:30 -0300

    feature-5

--- src/pages/Promotion/Search/Search.js
+++ src/pages/Promotion/Search/Search.js
@@ -1,10 +1,7 @@
-import React from 'react';
+import React from "react";
 import PromotionCard from "components/Promotion/Card/Card";
 
-
-const App = () => {
-
-  const promotion = {
+const promotion = {
     "id": 1,
     "title": "Kit Notebook Acer Aspire 3 + Mochila Green, A315-41-R790, AMD Ryzen 3 2200U Dual Core",
     "price": 1799,
@@ -18,17 +15,17 @@ const App = () => {
     ]
   }
 
-  return (
-    <div 
-      className="App" 
-      style={{
-        maxWidth: 800,
-        margin: '30px auto'
-      }}
-    >
+const PagesPromotionSearch = () => {
+    return (
+        <div       
+            style={{
+                maxWidth: 800,
+                margin: '30px auto'
+            }}
+        >
       <PromotionCard promotion={promotion}/>
     </div>
-  );
+    )
 }
 
-export default App;
+export default PagesPromotionSearch
