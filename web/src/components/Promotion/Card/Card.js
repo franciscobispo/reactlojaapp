6d8efbee891805b336485eb80e39b39af2cdba62
Francisco Cruz <francisco.bispocruz@gmail.com>
2020-08-27 13:33:05 -0300

    modal comentários

--- web/src/components/Promotion/Card/Card.js
+++ web/src/components/Promotion/Card/Card.js
@@ -2,7 +2,7 @@ import React from "react"
 import { Link } from "react-router-dom"
 import "./Card.css";
 
-const PromotionCard = ({promotion}) => (
+const PromotionCard = ({promotion, onClickComments}) => (
     <div className="promotion-card">
         <img src={promotion.imageUrl} alt={promotion.title} className="promotion-card__image"></img>
         <div className="promotion-card__info">
@@ -13,12 +13,21 @@ const PromotionCard = ({promotion}) => (
                     <div>"{promotion.comments[0].comment}"</div>
                 )}
 
-                <div className="promotion-card__comments-count">
+                <button className="promotion-card__comments-count" onClick={onClickComments}>
                     {promotion.comments.length}{' '}
                     comentário{promotion.comments.length > 1 ? 's' : ''}
-                </div>
-                <a href={promotion.url} target="_blank" rel="noopener noreferrer" className="promotion-card__link">IR PARA O SITE</a>
-                <Link to={`/edit/${promotion.id}`}>Editar</Link>
+                </button>
+                <a 
+                    href={promotion.url} 
+                    target="_blank" 
+                    rel="noopener noreferrer" 
+                    className="promotion-card__link"
+                >
+                    Ir Para o Site
+                </a>
+                <Link to={`/edit/${promotion.id}`} className="promotion-card__edit-button">
+                    Editar
+                </Link>
             </footer>
         </div>
     </div>
--- web/src/components/Promotion/List/List.js
+++ web/src/components/Promotion/List/List.js
@@ -1,9 +1,10 @@
-import React from "react"
+import React, { useState } from "react"
 import PromotionCard from "components/Promotion/Card/Card";
+import UIModal from 'components/UI/Modal/Modal'
 import './List.css'
 
-const PromotionList = ({ loading, error, promotions }) => {
-    console.log({ loading, error, promotions });
+const PromotionList = ({ loading, error, promotions }) => {   
+    const [promotionId, setPromotionId] = useState(null) 
     if (error) {
         return <div>Algo de errado não está certo</div>;
     }
@@ -18,8 +19,14 @@ const PromotionList = ({ loading, error, promotions }) => {
     return (
         <div className="promotion-list">
             {promotions.map((promotion) => (
-                <PromotionCard promotion={promotion} />
+                <PromotionCard 
+                    promotion={promotion} 
+                    onClickComments={() => setPromotionId(promotion.id)}
+                />
             ))}
+            <UIModal isOpen={Boolean(promotionId)} onClickClose={() => setPromotionId(null)}>
+                <h1>Comentários</h1>
+            </UIModal>
         </div>
     );
 }
