314e33fd7b45f7ad9630e47a846acb46eeec97cb
Francisco Cruz <francisco.bispocruz@gmail.com>
2020-08-31 13:45:15 -0300

    implementação infinitescroll

--- web/src/components/Promotion/Search/Search.js
+++ web/src/components/Promotion/Search/Search.js
@@ -1,35 +1,64 @@
 import React, { useEffect, useState, useRef } from "react"
 import useApi from 'components/utils/useApi'
+import UIInfiniteScroll from 'components/UI/InfiniteScroll/InfiniteScroll'
 import { Link } from 'react-router-dom'
 import PromotionList from "components/Promotion/List/List";
 import './Search.css'
 
+const baseParams = {
+    _embed: 'comments',
+    _order: 'desc',
+    _sort: 'id',
+    _limit: 3,
+}
+
 const PromotionSearch = () => {
+    const [page, setPage] = useState(1)
     const mountRef = useRef(null)
     const [search, setSearch] = useState('');
     const [load, loadInfo] = useApi({
         debounceDelay: 300,
         url: '/promotions',
         method: 'get',
-        params: {
-            _embed: 'comments',
-            _order: 'desc',
-            _sort: 'id',
-            title_like: search || undefined,
-        },
     });
 
     useEffect(() => {
         load({
-            debounced: mountRef.current
+            debounced: mountRef.current,
+            params: {
+                ...baseParams,
+                _page: 1,
+                title_like: search || undefined,
+            },
         });
-        
-        if(!mountRef.current) {
+
+        if (!mountRef.current) {
             mountRef.current = true
         }
 
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [search]);
+
+    function fetchMore() {
+        const newPage = page + 1
+        load({
+            isFetchMore: true,
+            params: {
+                ...baseParams,
+                _page: newPage,
+                title_like: search || undefined,
+            },
+            updateRequestInfo: (newRequestnfo, prevRequestInfo) => ({
+                ...newRequestnfo,
+                data: [
+                    ...prevRequestInfo.data,
+                    ...newRequestnfo.data
+                ]
+            })
+        })
+
+        setPage(newPage)
+    }
 
     return (
         <div className="promotion-search">
@@ -49,6 +78,13 @@ const PromotionSearch = () => {
                 loading={loadInfo.loading}
                 error={loadInfo.error}
             />
+
+            {loadInfo.data && 
+             !loadInfo.loading && 
+             loadInfo.data?.length < loadInfo.total && (
+                <UIInfiniteScroll fetchMore={fetchMore} />
+            )}
+
         </div>
     );
 };
